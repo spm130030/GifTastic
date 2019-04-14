f2d3aa3fd99480dd689df13e85e0429eea9bbceb
serena mallavarapu <serena.mallavarapu@gmail.com>
2019-04-13 19:48:54 -0500

    first commit

---
+++ assets/javascript/giphy.js
@@ -0,0 +1,73 @@
+var topics = ["Spongebob Squarepants", "Rugrats", "Courage The Cowardly Dog"];
+var numberOfGIFs = 10;
+var cutOffRating = "PG-13";
+
+function renderButtons(){
+	for(var i = 0; i < topics.length; i++) {
+		var newButton = $("<button>");
+		newButton.addClass("btn");
+		newButton.addClass("cartoon-button");
+		newButton.text(topics[i]);
+		$("#button-container").append(newButton);
+	}
+	$(".cartoon-button").unbind("click");
+
+	$(".cartoon-button").on("click", function(){
+		$(".gif-image").unbind("click");
+		$("#gif-container").empty();
+		$("#gif-container").removeClass("dotted-border");
+		populateGIFContainer($(this).text());
+	});
+
+}
+
+function addButton(show){
+	if(topics.indexOf(show) === -1) {
+		topics.push(show);
+		$("#button-container").empty();
+		renderButtons();
+	}
+}
+
+function populateGIFContainer(show){
+	$.ajax({
+		url: "https://api.giphy.com/v1/gifs/search?q=" + show + 
+		"&api_key=F0BFWTXxxKL8Qoc4kmSFS07erkaFZiK1&rating=" + cutOffRating + "&limit=" + numberOfGIFs,
+		method: "GET"
+	}).then(function(response){
+		response.data.forEach(function(element){
+			newDiv = $("<div>");
+			newDiv.addClass("individual-gif-container");
+			newDiv.append("<p>Rating: " + element.rating.toUpperCase() + "</p>");
+			var newImage = $("<img src = '" + element.images.fixed_height_still.url + "'>");
+			newImage.addClass("gif-image");
+			newImage.attr("state", "still");
+			newImage.attr("still-data", element.images.fixed_height_still.url);
+			newImage.attr("animated-data", element.images.fixed_height.url);
+			newDiv.append(newImage);
+			$("#gif-container").append(newDiv);
+		});
+		
+		$("#gif-container").addClass("dotted-border");
+		$(".gif-image").unbind("click");
+		$(".gif-image").on("click", function(){
+			if($(this).attr("state") === "still") {
+				$(this).attr("state", "animated");
+				$(this).attr("src", $(this).attr("animated-data"));
+			}
+			else {
+				$(this).attr("state", "still");
+				$(this).attr("src", $(this).attr("still-data"));
+			}
+		});
+	});
+}
+
+$(document).ready(function(){
+	renderButtons();
+	$("#submit").on("click", function(){
+		event.preventDefault();
+		addButton($("#cartoon-show").val().trim());
+		$("#cartoon-show").val("");
+	});
+});
